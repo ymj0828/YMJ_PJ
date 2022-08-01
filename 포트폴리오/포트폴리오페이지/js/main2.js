cef711bb82965ac7c9ddfeba283e1f90e1452edf
ymj0828 <ymj369@nate.com>
2022-08-01 17:53:19 +0900

    오늘 처음 푸시

--- 포트폴리오/포트폴리오페이지/js/main2.js
+++ 포트폴리오/포트폴리오페이지/js/main2.js
@@ -1,8 +1,152 @@
 ////// 제이쿼리 블록 /////////////////
+
+let bestdata = {
+    "eu": {
+        "사진1": "1-1",
+        "패키지1": "패키지",
+        "여행지1": "스위스/이탈리아 9일",
+        "해시태그1": "#나폼소 #피렌체 #베네치아 #스위스 여왕의 산 리기",
+        "금액1": "2,299,000원~",
+        "사진2": "1-2",
+        "패키지2": "패키지",
+        "여행지2": "[1개국 베스트셀러] 스위스 일주 ",
+        "해시태그2": "#시내호텔 1박 이상 #고르너그라트 or 리기산 등정 #여유로운 일정",
+        "금액2": "3,499,000원~",
+        "사진3": "1-3",
+        "패키지3": "패키지",
+        "여행지3": "[1개국 깊이보기] 스페인 일주 9일",
+        "해시태그3": "#5성1박 #전일정4성호텔 #맛10 #마드리드시내호텔1박 #루프탑레스토랑 #플라멩고",
+        "금액3": "3,699,000원~",
+    },
+    "dong": {
+        "사진1": "2-1",
+        "패키지1": "패키지",
+        "여행지1": "보라카이 4일,5일 #라카멜라 신관",
+        "해시태그1": "#전신마사지 #세일링보트 #디몰 #특식2회",
+        "금액1": "399,000원~",
+        "사진2": "2-2",
+        "패키지2": "패키지",
+        "여행지2": "[부산출발] 다낭 4일 #4성급호텔",
+        "해시태그2": "#바나산 #호이안투어 #오전자유 #쇼핑3회",
+        "금액2": "399,000원~",
+        "사진3": "2-3",
+        "패키지3": "패키지",
+        "여행지3": "방콕/파타야 5일  #파타야 5성호텔 ",
+        "해시태그3": "#가성비여행 #요트크루즈 #스노클링 #전통마사지",
+        "금액3": "699,000원~",
+    },
+    "guam": {
+        "사진1": "3-1",
+        "패키지1": "패키지",
+        "여행지1": "하얏트 리젠시 4일,5일",
+        "해시태그1": "#전객실오션프론트 #NEW시내관광 #스페셜런치 #귀국시코로나검사비용지원 ",
+        "금액1": "1,329,000원~",
+        "사진2": "3-2",
+        "패키지2": "에어텔, 패키지",
+        "여행지2": "두짓타니 호텔 4,5일",
+        "해시태그2": "#전객실오션프론트 #NEW시내관광 #에메랄드밸리 #귀국시코로나검사비용지원 ",
+        "금액2": "1,619,000원~",
+        "사진3": "3-3",
+        "패키지3": "패키지",
+        "여행지3": "롯데호텔 4일,5일",
+        "해시태그3": "#오션뷰룸 #호텔조식 #인피니트풀 #카바나이용권 #귀국시코로나검사비용지원 ",
+        "금액3": "1,109,000원~",
+    },
+    "sai": {
+        "사진1": "4-1",
+        "패키지1": "에어텔, 패키지",
+        "여행지1": "사이판 켄싱턴 4,5일 #오션뷰 ",
+        "해시태그1": "#올인클루시브 #키즈룸도 있어요",
+        "금액1": "1,209,000원~",
+        "사진2": "4-2",
+        "패키지2": "에어텔, 패키지",
+        "여행지2": "사이판 PIC 4,5일 #액티비티천국",
+        "해시태그2": "#PIC영어클래스 #가족전용상품 포함",
+        "금액2": "709,000원~",
+        "사진3": "4-3",
+        "패키지3": "패키지",
+        "여행지3": "월드리조트 4,5일",
+        "해시태그3": "#현지 PCR검사 지원 #가족전용상품 포함",
+        "금액3": "809,000원~",
+    },
+    "jp": {
+        "사진1": "5-1",
+        "패키지1": "패키지",
+        "여행지1": "오사카/고베 3일 #시내중심호텔",
+        "해시태그1": "#가성비추천 #도톤보리관광 ",
+        "금액1": "549,900원~",
+        "사진2": "5-2",
+        "패키지2": "패키지",
+        "여행지2": "도쿄/요코하마/오다이바 3일",
+        "해시태그2": "#도쿄시내숙박 #라면박물관",
+        "금액2": "703,880원~",
+        "사진3": "5-3",
+        "패키지3": "패키지",
+        "여행지3": "규슈 3일 #시내호텔숙박",
+        "해시태그3": "#베스트셀러 #후쿠오카핵심관광",
+        "금액3": "489,900원~",
+    }
+};
+
+
 $(() => {
 
     // 호출확인
     console.log("로딩완료!");
+
+    $(".bestTravel span").click(function(){
+
+        // 1. 버튼텍스트 읽어오기
+        let btxt = $(this).attr('class');
+        console.log("버튼:",btxt);
+
+        let data = bestdata[btxt];
+        console.log("데이터:",data);
+
+        $('#content2 .main .bestTravel span').removeClass('on');
+
+        $(this).addClass('on')
+
+        for(let x in data){
+
+        switch(x){
+            case "사진1": $("#bestText1 .img").html(`<img src="../images/content2_${data[x]}.jpg" alt="포스터">`);
+                break;
+            case "패키지1": $('#bestText1 .text1 span').text(data[x]);
+                break;
+            case "여행지1": $('#bestText1 .text2 strong').text(data[x]);
+                break;
+            case "해시태그1": $('#bestText1 .text2 p').text(data[x]);
+                break;
+            case "금액1": $('#bestText1 .text3 strong').text(data[x]);
+                break;
+            case "사진2": $("#bestText2 .img").html(`<img src="../images/content2_${data[x]}.jpg" alt="포스터">`);
+                break;
+            case "패키지2": $('#bestText2 .text1 span').text(data[x]);
+                break;
+            case "여행지2": $('#bestText2 .text2 strong').text(data[x]);
+                break;
+            case "해시태그2": $('#bestText2 .text2 p').text(data[x]);
+                break;
+            case "금액2": $('#bestText2 .text3 strong').text(data[x]);
+                break;
+            case "사진3": $("#bestText3 .img").html(`<img src="../images/content2_${data[x]}.jpg" alt="포스터">`);
+                break;
+            case "패키지3": $('#bestText3 .text1 span').text(data[x]);
+                break;
+            case "여행지3": $('#bestText3 .text2 strong').text(data[x]);
+                break;
+            case "해시태그3": $('#bestText3 .text2 p').text(data[x]);
+                break;
+            case "금액3": $('#bestText3 .text3 strong').text(data[x]);
+                break;
+        }
+
+    }
+
+    });
+
+
 
     // 이벤트 대상: .abtn
     // 이벤트 : click() 메서드 사용
@@ -14,9 +158,6 @@ $(() => {
     // 변경에 사용할 제이쿼리 메서드: 
     // animate({CSS속성},시간,이징,함수)
 
-    // 변경대상: 블릿 - .indic li
-    let indic = $(".indic li");
-
     // 광클 금지상태변수
     let prot = 0; // 1-불허용, 0-허용
 
@@ -26,7 +167,15 @@ $(() => {
     // 애니메이션 이징 변수
     const aniE = "easeInOutQuint";
 
+    let pause = $(".pause");
+
+    let isPlay = false;
+
+
     $(".abtn").click(function () {
+
+        
+
 
         // console.log("진입:",prot);
 
@@ -43,13 +192,15 @@ $(() => {
 
         // 1. 오른쪽여부
         // is(클래스/아이디명) -> 선택요소해당여부 리턴
-        let isR = $(this).is(".ab2");
+        let isNext = $(this).is(".next");
+        let isPrev = $(this).is(".prev");
 
         // 호출확인(방향확인)
-        // console.log("오른쪽버튼인가? ",isR);
+        // console.log("오른쪽버튼인가? ",isNext);
 
         // 2. 버튼별 분기하여 기능구현
-        if (isR) { // 오른쪽버튼
+        if (isNext) { // 오른쪽버튼
+
 
             slide.animate({
                     left: "-100%"
@@ -67,11 +218,46 @@ $(() => {
                         .css({
                             left: "0"
                         });
+
+
+                    $("li",this).removeClass('on')
+                    $("li",this).first().addClass('on');
+
+                    switch($("#slide").children().attr('class')){
+                        case "bg1 on":  $('#content1').css({backgroundColor:"#cdf1eb"})
+                        $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                            break
+                        case "bg2 on":  $('#content1').css({backgroundColor:"#cff0fa"})
+                        $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                            break
+                        case "bg3 on":  $('#content1').css({backgroundColor:"#e9dbce"})
+                        $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                            break
+                        case "bg4 on":  $('#content1').css({backgroundColor:"#f5dbda"})
+                        $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                            break
+                        case "bg5 on":  $('#content1').css({backgroundColor:"#cae5f8"})
+                        $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                            break
+                        case "bg6 on":  $('#content1').css({backgroundColor:"#d3f2df"})
+                        $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                            break
+                        case "bg7 on":  $('#content1').css({backgroundColor:"#d0cef6"})
+                        $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                            break
+                        case "bg8 on":  $('#content1').css({backgroundColor:"#cff0fa"})
+                        $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                            break
+    
+                    }
+
+
                     // 동시에 left값을 0으로 변경!
                 }); ///////// animate ///////
 
+
         } /////////// if ///////////
-        else { // 왼쪽버튼
+        else if(isPrev) { // 왼쪽버튼
 
             // 맨뒤요소를 맨앞에 이동
             slide
@@ -89,72 +275,56 @@ $(() => {
                         left: "0"
                     },
                     aniT, //시간
-                    aniE // 이징
+                    aniE, // 이징
+                    
                 ); ////// animate //////
 
-        } /////////// else ///////////
+                $("li",$('#slide')).removeClass('on')
+                $("li",$('#slide')).first().addClass('on');
 
-        // 3. 등장슬라이드와 같은 순번의 블릿변경하기
-        // 변경내용: 블릿 li에 class="on"을 주고
-        // 나머지 li에는 class="on"을 지운다!
+                switch($("#slide").children().attr('class')){
+                    case "bg1 on":  $('#content1').css({backgroundColor:"#cdf1eb"})
+                    $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                        break
+                    case "bg2 on":  $('#content1').css({backgroundColor:"#cff0fa"})
+                    $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                        break
+                    case "bg3 on":  $('#content1').css({backgroundColor:"#e9dbce"})
+                    $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                        break
+                    case "bg4 on":  $('#content1').css({backgroundColor:"#f5dbda"})
+                    $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                        break
+                    case "bg5 on":  $('#content1').css({backgroundColor:"#cae5f8"})
+                    $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                        break
+                    case "bg6 on":  $('#content1').css({backgroundColor:"#d3f2df"})
+                    $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                        break
+                    case "bg7 on":  $('#content1').css({backgroundColor:"#d0cef6"})
+                    $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                        break
+                    case "bg8 on":  $('#content1').css({backgroundColor:"#cff0fa"})
+                    $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                        break
 
-        // 같은 순번 슬라이드는 
-        // 오른쪽일때 2번째 슬라이드(순번1)
-        // 왼쪽일때 1번째 슬라이드(순번0)
-        // eq(순번) -> 몇번째 요소
-        // eq(isR?1:0) -> isR?1:0 -> 3항연산자
-        // isR이 true이면(1이면) 1을 출력, 아니면 0출력
-        let sseq =
-            slide.find("li").eq(isR ? 1 : 0).attr("data-seq");
-        console.log("슬순:", sseq);
-
-        // 등장슬라이드 순번과 동일한 블릿순번에
-        // 클래스 "on"주기
-        // 제이쿼리 클래스주기 메서드는? addClass(클명)
-        // 제이쿼리 클래스 컨트롤 메서드
-        // 1. addClass()
-        // 2. removeClass()
-        // 3. toggleClass()
-
-        // 변경대상: .indic li -> indic 변수
-
-        // 해당순번(sseq)의 블릿li에 클래스"on" 넣기 
-        indic.eq(sseq).addClass("on")
-            // 다른형제요소들 -> siblings() 은 클래스지워!
-            .siblings().removeClass("on");
+                }
+        } 
+        
+        else {
+            
+            if(isPlay==false){
+                stopClearAuto();
+                pause.addClass('play')
+                isPlay=true;
+            }
+            else{
+                pause.removeClass('play')
+                isPlay=false;
+            }
+        }
 
     }); /////////// click /////////////
-
-
-    // 블릿순번을 결정하기 위한 슬라이드 고유번호
-    // 새로운 속성 만들기!!!
-    // 새로운 속성은 'data-' 라는 이름으로 시작하면
-    // 만들 수 있다!(w3c공식문법)
-    // 우리는 각 슬라이드에 'data-seq'라는 이름의
-    // 순번 저장용 속성을 만들고자 한다!
-    // 이때 사용할 제이쿼리 메서드는 바로~~~~~~~
-    // each() 메서드 - for문 안써도됨!
-    // each((idx,ele)=>{})
-    // idx -> 첫번째 전달변수 : 순번
-    // ele -> 두번째 전달변수 : 요소자신
-
-    // 속성넣기 제이쿼리 메서드는? attr(속성명,값)
-    // 비교) JS의 속성셋팅은 setAttribute(속성명,값)
-    // 비교) JS의 속성읽기는 getAttribute(속성명)
-    // 제이쿼리는 하나로 다 됨~!!!!
-    // attr(속성명) -> 속성값 읽기
-    // attr(속성명, 값) -> 속성값 셋팅
-
-
-    // 대상: 슬라이드의 li
-    slide.find("li").each(function (idx, ele) {
-
-        // 'data-seq' 라는 
-        // 새로운 속성에 순번을 넣음!
-        $(ele).attr("data-seq", idx);
-        // console.log(ele,idx);
-
-    }); ///////// each ////////////////
 
 
     // 배너 자동호출 넘기기 셋팅 //////
@@ -188,17 +358,45 @@ $(() => {
                         .css({
                             left: "0"
                         });
+
+                        $("li",$('#slide')).removeClass('on')
+                        $("li",$('#slide')).first().addClass('on');
+        
+                        switch($("#slide").children().attr('class')){
+                            case "bg1 on":  $('#content1').css({backgroundColor:"#cdf1eb"})
+                            $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                                break
+                            case "bg2 on":  $('#content1').css({backgroundColor:"#cff0fa"})
+                            $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                                break
+                            case "bg3 on":  $('#content1').css({backgroundColor:"#e9dbce"})
+                            $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                                break
+                            case "bg4 on":  $('#content1').css({backgroundColor:"#f5dbda"})
+                            $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                                break
+                            case "bg5 on":  $('#content1').css({backgroundColor:"#cae5f8"})
+                            $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                                break
+                            case "bg6 on":  $('#content1').css({backgroundColor:"#d3f2df"})
+                            $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                                break
+                            case "bg7 on":  $('#content1').css({backgroundColor:"#d0cef6"})
+                            $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                                break
+                            case "bg8 on":  $('#content1').css({backgroundColor:"#cff0fa"})
+                            $('.control span').text($('#slide li').attr('class').charAt(2)+'/8')
+                                break
+        
+                        }
+
+
+
                 }); ///// animate /////
-
-            // 블릿변경하기
-            let sseq =
-            slide.find("li").eq(1).attr("data-seq");
-
-            indic.eq(sseq).addClass("on")
-                .siblings().removeClass("on");
 
 
         }, 3000); ///// 인터발함수 ///
+
 
     } ////////// autoSlide함수 ///////
 
@@ -225,7 +423,14 @@ $(() => {
     // 제이쿼리 trigger(이벤트명) 메서드
     // ->  선택요소에 강제 이벤트발생 메서드
 
+    function stopClearAuto(){
+        // 인터발지우기
+        clearInterval(autoI);
+        // 타임아웃지우기(실행쓰나미방지!)
+        clearTimeout(autoT);
+    }
 
 
-}); ////////////////// jQB ////////////////////
-///////////////////////////////////////////////
+
+
+}); 
